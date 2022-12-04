9252ad7af300cac53ebed67c83c0039d09de774f
wizardforcel <562826179@qq.com>
2022-12-04 19:39:23 +0800

    2022-12-04 19:39:23

--- imgyaso/__init__.py
+++ imgyaso/__init__.py
@@ -4,7 +4,7 @@
 https://github.com/apachecn/imgyaso"""
 
 from .adathres import adathres_bts, adathres
-from .dither import grid_bts, grid, noise_bts, noise
+from .dither import grid_bts, grid, noise_bts, noise, noisebw_bts, noisebw
 from .quant import pngquant_bts, pngquant
 from .trunc import trunc_bts, trunc
 from .util import *
